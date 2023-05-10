fbca8b0d15a0d228dd339231e0d4d3b82b778071
Ubuntu <ubuntu@ip-172-31-41-124.sa-east-1.compute.internal>
2023-05-10 17:13:50 +0000

    Projeto finalizado

---
+++ src/updateitem.js
@@ -0,0 +1,26 @@
+"use strict";
+
+const AWS = require("aws-sdk")
+
+const updateItem = async (event) => {
+
+	const {itemStatus} = JSON.parse(event.body);
+	const {id} = event.pathParameters
+
+	const dynamoDB = new AWS.DynamoDB.DocumentClient();
+
+	await dynamoDB.update({
+		TableName: "ItemTableNew",
+		Key: {id},
+		UpdateExpression: 'set itemStatus = :itemStatus',
+		ExpressionAttributeValues: {
+			':itemStatus': itemStatus
+		},
+		ReturnValues: "ALL_NEW"
+	}).promise()
+
+}
+
+module.exports = {
+	handler:updateItem
+}
